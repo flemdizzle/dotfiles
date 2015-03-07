225bc44497e9061ff1cfb93cc52cae9ff1ef77b7
flemdizzle <fleminsh@gmail.com>
2015-03-07 01:13:40 -0500

    copied in the base layer for my dotfiles and got the brew-cask.sh file looking the way that I want it

---
+++ brew-cask.sh
@@ -0,0 +1,28 @@
+# Install native apps
+
+brew install caskroom/cask/brew-cask
+brew tap caskroom/versions
+
+# daily
+brew cask install alfred
+brew cask install dropbox
+brew cask install flux
+brew cask install caffeine
+brew cask install spectacle
+brew cask install evernote
+
+# dev
+brew cask install iterm2
+brew cask install sublime-text
+brew cask install mou
+
+# fun
+brew cask install limechat
+
+# browsers
+brew cask install google-chrome-canary
+brew cask install firefox-nightly --force
+brew cask install webkit-nightly --force
+brew cask install chromium --force
+
+
