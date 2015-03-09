8b58d4158d8b1ed6f56866e43a5f97a0fc264a2f
flemdizzle <fleminsh@gmail.com>
2015-03-09 11:08:51 -0400

    added a comment to find a way to hide the finder icon

--- osx-config.sh
+++ osx-config.sh
@@ -8,6 +8,9 @@ defaults write com.apple.dock pinning end;killall Dock
 # Add a spacer to the right side of the Dock (where the Trash is)
 defaults write com.apple.dock persistent-others -array-add '{tile-data={}; tile-type="spacer-tile";}'
 
+# Hides the finder icon on the dock
+
+
 ###############################################################################
 # Terminal Settings                                                           #
 ###############################################################################
